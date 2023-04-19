a025d475734d4d7bab5076d43a3c2fe1df4378e2
sircuspelle <al.sol.1935@gmail.com>
2023-04-19 23:36:05 +0300

    титанические изменения во имя карточного героя

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from flask_login import login_required, LoginManager
 from data import db_session
 
 from data.users import User
-from data.cards import Card
+from data.cards import Card, Card_Page
 
 from forms.authorizer_forms import RegisterForm
-from forms.card_form import CardsForm
+
+from forms.card_form import MainCardsForm, SmallCardsForm
+from forms.pre_create_form import PreCreateForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -24,8 +26,22 @@ def load_user(user_id):
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
+    cards = db_sess.query(Card).all()
+    if len(cards) > 5:
+        cards = cards[:5]
+    return render_template("index.html", title='indexpage', cards=cards)
 
-    return render_template("index.html", title='indexpage', cards=)
+@app.route("/news")
+def show_news():
+    return """Тут пока пусто, но мы уже почти это исправили"""
+
+@app.route("/cards")
+def show_cards():
+    db_sess = db_session.create_session()
+    cards = db_sess.query(Card).all()
+    return render_template("cards.html", title='catalogue', cards=cards)
+
+
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -53,15 +69,33 @@ def reqister():
         return redirect('/index')
     return render_template('register.html', title='Регистрация', form=form)
 
+cards = None
+points = 0
+@app.route('/new_card_pre',  methods=['GET', 'POST'])
+def start_to_add_cards():
+    pre_form = PreCreateForm()
+    if pre_form.validate_on_submit():
 
-@app.route('/new_card',  methods=['GET', 'POST'])
-def add_cards():
-    form = CardsForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
+        global cards, points
         cards = Card()
+        cards.points_count = pre_form.points_count.data
+        cards.region = pre_form.region.data
+
+        points = [i for i in range(1, cards.points_count * 2 + 1)]
+
+        return redirect('/new_card')
+    return render_template('pre_card.html', title='Добавление Маршрута',
+                           form=pre_form)
+
+@app.route('/new_card', methods=['GET', 'POST'])
+def add_cards():
+    global cards, points
+    form = MainCardsForm()
+    if form.validate_on_submit():
+
+        db_sess = db_session.create_session()
+
         cards.title = form.title.data
-        cards.region = form.region.data
         cards.place = form.place.data
         cards.longest = form.longest.data
         #####
@@ -69,9 +103,37 @@ def add_cards():
 
         db_sess.add(cards)
         db_sess.commit()
-        return redirect('/')
-    return render_template('card.html', title='Добавление Маршрут',
+        return redirect('/create_card/1')
+    return render_template('main_card.html', title='Добавление Маршрута', count=points,
                            form=form)
+
+@app.route('/create_card/<int:number>', methods=['GET', 'POST'])
+def add_page(number):
+    global cards, points
+    form = SmallCardsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        card = Card_Page()
+        card.txt = form.text.data
+        card.title = form.title.data
+        card.picture = form.picture.data
+
+        #####
+        card.mother = 2
+
+        db_sess.add(card)
+        db_sess.commit()
+
+        if number != points[-1]:
+            return redirect(f'/create_card/{number + 1}')
+        else:
+            return redirect('/')
+    return render_template('small_card.html', title=f'Добавление Маршрута{number}', count=points,
+                           form=form)
+
+@app.route("/display_card/<int:number>")
+def display_card():
+    return 'fiasko'
 
 def main():
     db_session.global_init("db/blogs.db")
