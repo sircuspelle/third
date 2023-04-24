949c8ef34e4575c9c623837b0ed7655500050320
Danya <dcekmarev@yandex.ru>
2023-04-25 01:58:26 +0300

    вроде удачно объединил 2 ветки

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ from data import db_session
 
 from data.users import User
 from data.cards import Card, Card_Page
+from data.news import News
 
 from forms.authorizer_forms import RegisterForm, LoginForm
 
 from forms.card_form import MainCardsForm, SmallCardsForm
 from forms.pre_create_form import PreCreateForm
+from forms.news import NewsForm
+from forms.forum import ForumForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+username = ''
 
 
 @login_manager.user_loader
@@ -37,12 +43,122 @@ def index():
     cards = db_sess.query(Card).all()
     if len(cards) > 5:
         cards = cards[:5]
-    return render_template("index.html", title='indexpage', cards=cards)
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(
+            (News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", cards=cards, news=news)
 
 
-@app.route("/news")
-def show_news():
-    return """Тут пока пусто, но мы уже почти это исправили"""
+@app.route('/news',  methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = News()
+        news.title = form.title.data
+        if len(form.content.data) > 120:
+            news.preview = form.content.data[:120:] + "..."
+            news.content = form.content.data
+        else:
+            news.content = form.content.data
+        news.is_private = form.is_private.data
+        current_user.news.append(news)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('news.html', title='Добавление новости',
+                           form=form)
+
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            news.title = form.title.data
+            if len(form.content.data) > 120:
+                news.preview = form.content.data[:120:] + "..."
+                news.content = form.content.data
+            else:
+                news.preview = ''
+                news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('news.html',
+                           title='Редактирование новости',
+                           form=form
+                           )
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id,
+                                      News.user == current_user
+                                      ).first()
+    if news:
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+@app.route('/reading_news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def reading_news(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id,
+                                      News.user == current_user
+                                      ).first()
+    return render_template('reading_news.html', news=news)
+
+
+@app.route('/forum', methods=['GET', 'POST'])
+@login_required
+def forum():
+    global username
+    form = ForumForm()
+    if username:
+        form.content.data = f'@{username}, '
+    if form.submit.data:
+        with open('files/forum0.txt', "a", encoding="utf8") as file:
+            file.write(f"{form.content.data};{current_user.nickname};{current_user.id}\n")
+    with open('files/forum0.txt', "r", encoding="utf8") as file:
+        content = file.readlines()
+        content = [i.rsplit(';', 2) for i in content]
+    return render_template('forum.html', title='Форум', form=form, content=content)
+
+
+@app.route('/forum/<nickname>', methods=['GET', 'POST'])
+@login_required
+def select_name(nickname):
+    global username
+    username = nickname
+    return redirect('/forum')
 
 
 @app.route("/cards")
