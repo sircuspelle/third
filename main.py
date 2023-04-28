7fce40b3c1d8f1d465b1835510a0b1e16909827a
sircuspelle <al.sol.1935@gmail.com>
2023-04-28 21:45:09 +0300

    тут перерисовано гоавным образом

--- main.py
+++ main.py
@@ -15,29 +15,10 @@ from forms.pre_create_form import PreCreateForm
 from forms.news import NewsForm
 from forms.forum import ForumForm
 
+from help_functions import region_coords
+
 import requests
 
-
-def region_coords(name):
-    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={name}&format=json"
-
-    # Выполняем запрос.
-    response = requests.get(geocoder_request)
-    if response:
-        # Преобразуем ответ в json-объект
-        json_response = response.json()
-
-        # Получаем первый топоним из ответа геокодера.
-        # Согласно описанию ответа, он находится по следующему пути:
-        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-        # Координаты центра топонима:
-        toponym_coodrinates = toponym["Point"]["pos"]
-
-        return ','.join(toponym_coodrinates.split()[::-1])
-    else:
-        print("Ошибка выполнения запроса:")
-        print(geocoder_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -65,13 +46,8 @@ def logout():
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
-    cards = sorted(db_sess.query(Card).all(), key=lambda x: x.loyality_counter, reverse=True)
-
-    # db_sess.close()
-
-    if len(cards) > 5:
-        cards = cards[:5]
-    return render_template("index.html", cards=cards)
+    cards = sorted(db_sess.query(Card).all(), key=lambda x: x.loyality_counter, reverse=True)[:5]
+    return render_template("index.html", cards=cards, title='Главная')
 
 
 @app.route('/news',  methods=['GET', 'POST'])
@@ -83,6 +59,12 @@ def all_news():
         news = db_sess.query(News).filter(News.is_private != True)
     return render_template("all_news.html", news=news, title='news')
 
+@app.route("/cards")
+def show_cards():
+    db_sess = db_session.create_session()
+    cards = sorted(db_sess.query(Card).all(), key=lambda x: x.loyality_counter, reverse=True)
+    return render_template("cards.html", title='Каталог', cards=cards)
+
 @app.route('/add_news',  methods=['GET', 'POST'])
 @login_required
 def add_news():
@@ -93,17 +75,15 @@ def add_news():
         news.title = form.title.data
         if len(form.content.data) > 120:
             news.preview = form.content.data[:120:] + "..."
-            news.content = form.content.data
-        else:
-            news.content = form.content.data
+        news.content = form.content.data
         news.is_private = form.is_private.data
         current_user.news.append(news)
         db_sess.merge(current_user)
         db_sess.commit()
+        db_sess.close()
         return redirect(f'/news')
     return render_template('news.html', title='Добавление новости',
                            form=form)
-
 
 @app.route('/edit_news/<int:news_id>', methods=['GET', 'POST'])
 @login_required
@@ -135,6 +115,7 @@ def edit_news(news_id):
                 news.content = form.content.data
             news.is_private = form.is_private.data
             db_sess.commit()
+            db_sess.close()
             return redirect(f'/news')
         else:
             abort(404)
@@ -154,27 +135,31 @@ def news_delete(news_id):
     if news:
         db_sess.delete(news)
         db_sess.commit()
+        db_sess.close()
     else:
         abort(404)
-    return redirect(f'/news')
+        db_sess.close()
+    return redirect('/news')
 
 
 @app.route('/reading_news/<int:news_id>', methods=['GET', 'POST'])
-@login_required
 def reading_news(news_id):
     db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.id == news_id,
-                                      News.user == current_user
-                                      ).first()
+    news = db_sess.query(News).filter(News.id == news_id).first()
     return render_template('reading_news.html', news=news)
 
 
 @app.route('/card_<int:card_id>/news',  methods=['GET', 'POST'])
 def card_news(card_id):
     db_sess = db_session.create_session()
-    news = db_sess.query(News).filter((News.card_id == card_id), (News.is_private != True))
-    card = db_sess.query(Card).filter(Card.id == card_id).first()
-    return render_template("card_news.html", news=news, card=card)
+
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter((News.card_id == card_id), ((News.user == current_user) | (News.is_private != True))).all()
+    else:
+        news = db_sess.query(News).filter((News.card_id == card_id), (News.is_private != True)).all()
+
+    empty = bool(news)
+    return render_template("card_news.html", news=news, card_id=card_id, empty=empty)
 
 
 @app.route('/card_<int:card_id>/add_news',  methods=['GET', 'POST'])
@@ -239,6 +224,7 @@ def edit_card_news(card_id, news_id):
                            )
 
 
+# зачем
 @app.route('/card_<int:card_id>/news_delete/<int:news_id>', methods=['GET', 'POST'])
 @login_required
 def card_news_delete(card_id, news_id):
@@ -254,14 +240,13 @@ def card_news_delete(card_id, news_id):
     return redirect(f'/card_{card_id}/news')
 
 
-@app.route('/card_<int:card_id>/reading_news/<int:news_id>', methods=['GET', 'POST'])
-@login_required
-def reading_card_news(card_id, news_id):
-    db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.id == news_id,
-                                      News.user == current_user
-                                      ).first()
-    return render_template('reading_news.html', news=news)
+# @app.route('/card_<int:card_id>/reading_news/<int:news_id>', methods=['GET', 'POST'])
+# def reading_card_news(card_id, news_id):
+#     db_sess = db_session.create_session()
+#     news = db_sess.query(News).filter(News.id == news_id,
+#                                       News.user == current_user
+#                                       ).first()
+#     return render_template('reading_news.html', news=news)
 
 
 @app.route('/card_<int:card_id>/forum', methods=['GET', 'POST'])
@@ -291,16 +276,6 @@ def select_name(card_id, nickname):
     return redirect(f'/card_{card_id}/forum')
 
 
-@app.route("/cards")
-def show_cards():
-    db_sess = db_session.create_session()
-    cards = sorted(db_sess.query(Card).all(), key=lambda x: x.loyality_counter, reverse=True)
-
-    # db_sess.close()
-
-    return render_template("cards.html", title='Каталог', cards=cards)
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
@@ -313,7 +288,11 @@ def reqister():
         if db_sess.query(User).filter(User.email == form.email.data).first():
             return render_template('register.html', title='Регистрация',
                                    form=form,
-                                   message="Такой пользователь уже есть")
+                                   message="Пользователь с такой почтой уже есть")
+        if db_sess.query(User).filter(User.nickname == form.nickname.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пользователь с таким именем уже есть")
         user = User(
             nickname=form.nickname.data,
             region=form.region.data,
@@ -403,10 +382,54 @@ def add_cards():
     return render_template('main_card.html', title='Добавление Маршрута', count=points,
                            form=form, page=0)
 
+@app.route('/new_card/page/<int:number>', methods=['GET', 'POST'])
+def add_page(number):
+    global cards, points
+    form = SmallCardsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        card = Card_Page()
+        card.txt = form.text.data
+        card.title = form.title.data
+
+        file = form.picture.data
+        if file:
+            card.picture = f'{cards.id}_{number}.{file.filename.split(".")[-1]}'
+            file.save(f'static/img/{card.picture}')
+
+        card.mother = cards.id
+
+        db_sess.add(card)
+        db_sess.commit()
+
+        if number != points[-1]:
+            return redirect(f'/new_card/page/{number + 1}')
+        else:
+            db_sess.add(cards)
+            db_sess.commit()
+            db_sess.close()
+            return redirect('/')
+
+    if number % 2:
+        return render_template('small_card.html', title=f'шаг {number}', page=number,
+                               head=f'Расскажи о точке остановки №{number}',
+                               count=points,
+                               form=form)
+    else:
+        if number == points[-1]:
+            return render_template('small_card.html', title=f'шаг {number}', page=number,
+                                   head=f'Расскажи, как добираться в начало пути',
+                                   count=points, delta=True,
+                                   form=form)
+        return render_template('small_card.html', title=f'шаг {number}', page=number,
+                               head=f'Расскажи, как добирался от пункта №{number - 1} до следующей остановки',
+                               count=points,
+                               form=form)
+
 @app.route('/start_corrector/<int:id>', methods=['GET', 'POST'])
+@login_required
 def starter(id):
     global cards, points
-    form = MainCardsForm()
 
     db_sess = db_session.create_session()
     cards = db_sess.query(Card).filter(Card.id == id,
@@ -421,7 +444,6 @@ def starter(id):
     return redirect(f'/old_card_pre_map#{cards.map}')
 
 @app.route('/old_card_pre_map', methods=['GET', 'POST'])
-@login_required
 def start_to_chng_map():
     return render_template('pre_map.html', title='Изменение Карты', chng=True)
 
@@ -459,52 +481,6 @@ def change_cards(id):
 def size(el):
     return el.stream.seek(0, os.SEEK_END)
 
-
-@app.route('/new_card/page/<int:number>', methods=['GET', 'POST'])
-def add_page(number):
-    global cards, points
-    form = SmallCardsForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        card = Card_Page()
-        card.txt = form.text.data
-        card.title = form.title.data
-
-        file = form.picture.data
-        if file:
-            card.picture = f'static/img/{cards.id}_{number}.{file.filename.split(".")[-1]}'
-            file.save(card.picture)
-
-        card.mother = cards.id
-
-        db_sess.add(card)
-        db_sess.commit()
-
-        if number != points[-1]:
-            return redirect(f'/new_card/page/{number + 1}')
-        else:
-            db_sess.add(cards)
-            db_sess.commit()
-            db_sess.close()
-            return redirect('/')
-
-    if number % 2:
-        return render_template('small_card.html', title=f'шаг {number}', page=number,
-                               head=f'Расскажи о точке остановки №{number}',
-                               count=points,
-                               form=form)
-    else:
-        if number == points[-1]:
-            return render_template('small_card.html', title=f'шаг {number}', page=number,
-                                   head=f'Расскажи, как добираться в начало пути',
-                                   count=points, delta=True,
-                                   form=form)
-        return render_template('small_card.html', title=f'шаг {number}', page=number,
-                               head=f'Расскажи, как добирался от пункта №{number - 1} до следующей остановки',
-                               count=points,
-                               form=form)
-
-
 @app.route('/card/page/<int:number>', methods=['GET', 'POST'])
 def change_page(number):
     global cards, points
@@ -540,7 +516,6 @@ def change_page(number):
                 db_sess.add(cards)
                 db_sess.commit()
 
-                #
                 db_sess.close()
 
                 return redirect('/')
@@ -562,7 +537,6 @@ def change_page(number):
                                form=form)
 
 
-# heads_in_card = 0
 @app.route("/display_card/<int:id>")
 def load_card(id):
     global points, cards
@@ -576,8 +550,6 @@ def load_card(id):
         return redirect(f'/display_cards#{cards.map}')
 
     abort(404)
-
-
 
 @app.route("/display_cards")
 def display_card():
@@ -643,6 +615,7 @@ def cards_like(arg):
             card.creator_obj.loyality += 1
 
             db_sess.commit()
+            db_sess.close()
     else:
         abort(404)
 
